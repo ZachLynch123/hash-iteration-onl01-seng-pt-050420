8893709c886aaff7b0f21c6184be542626dfbdd5
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-13 18:11:03 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,9 +7,6 @@
 def happy_birthday(birthday_kids)
   birthday_kids.each do |name, age|
      puts"Happy Birthday #{name}! You are now #{age} years old!"
-   if age < 12
-     puts "You are too old for this."
-   end
    end
 end
 
